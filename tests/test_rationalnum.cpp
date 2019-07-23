292fefc95f5e25324def55a4239dce10dfd5e9e3
Paul Ngo <26474149+inthewaves@users.noreply.github.com>
2019-07-22 17:44:31 -0700

    Unit tests: fix possible memory leaks

--- tests/test_rationalnum.cpp
+++ tests/test_rationalnum.cpp
@@ -157,7 +157,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac0 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).add(0/5)" ) {
-            testFrac0.add(new bffrac::RationalNum(0,5));
+            auto testx0 = bffrac::RationalNum(0,5);
+            testFrac0.add(&testx0);
 
             THEN("we get nonegative 0/1") {
                 REQUIRE(!testFrac0.positive());
@@ -169,7 +170,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac1 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).add(22/15)" ) {
-            testFrac1.add(new bffrac::RationalNum(22,15));
+            auto testx1 = bffrac::RationalNum(22,15);
+            testFrac1.add(&testx1);
 
             THEN("we get 22/15") {
                 REQUIRE(testFrac1.positive());
@@ -181,7 +183,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac2 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).add(-22/15)" ) {
-            testFrac2.add(new bffrac::RationalNum(-22,15));
+            auto testx2 = bffrac::RationalNum(-22,15);
+            testFrac2.add(&testx2);
 
             THEN("we get -22/15") {
                 REQUIRE(!testFrac2.positive());
@@ -194,7 +197,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
     GIVEN( "2/3" ) {
         auto testAdditiveInverse1 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).add(-2/3) (additive inverse)" ) {
-            testAdditiveInverse1.add(new bffrac::RationalNum(-2,3));
+            auto testx0 = bffrac::RationalNum(-2,3);
+            testAdditiveInverse1.add(&testx0);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testAdditiveInverse1.positive());
@@ -205,19 +209,20 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
 
         auto testAdditiveInverse2 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).add(-16/24) (additive inverse)" ) {
-            testAdditiveInverse1.add(new bffrac::RationalNum(-16,24));
+            auto testx01 = bffrac::RationalNum(-16,24);
+            testAdditiveInverse2.add(&testx01);
 
             THEN("we get nonpositive 0/1") {
-                REQUIRE(!testAdditiveInverse1.positive());
-                REQUIRE(testAdditiveInverse1.getNumerator() == 0);
-                REQUIRE(testAdditiveInverse1.getDenominator() == 1);
+                REQUIRE(!testAdditiveInverse2.positive());
+                REQUIRE(testAdditiveInverse2.getNumerator() == 0);
+                REQUIRE(testAdditiveInverse2.getDenominator() == 1);
             }
         }
 
         auto testFrac1 = bffrac::RationalNum(2,3);
-
         WHEN( "we run (2/3).add(4/5)" ) {
-            testFrac1.add(new bffrac::RationalNum(4,5));
+            auto testx1 = bffrac::RationalNum(4,5);
+            testFrac1.add(&testx1);
 
             THEN("we get 22/15") {
                 REQUIRE(testFrac1.positive());
@@ -229,7 +234,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac2 = bffrac::RationalNum(2,3);
 
         WHEN( "we run (2/3).add(-4/5)" ) {
-            testFrac2.add(new bffrac::RationalNum(-4,5));
+            auto testx2 = bffrac::RationalNum(-4,5);
+            testFrac2.add(&testx2);
 
             THEN("we get -2/15") {
                 REQUIRE(!testFrac2.positive());
@@ -241,8 +247,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac3 = bffrac::RationalNum(2,3);
 
         WHEN( "we run (2/3).add(0/5)" ) {
-            auto testFrac3add = bffrac::RationalNum(0,5);
-            testFrac3.add(&testFrac3add);
+            auto testx3 = bffrac::RationalNum(0,5);
+            testFrac3.add(&testx3);
 
             THEN("we get 2/3") {
                 REQUIRE(testFrac3.positive());
@@ -255,9 +261,9 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
     GIVEN( "-2/3" ) {
 
         auto testAdditiveInverse1 = bffrac::RationalNum(-2,3);
-
         WHEN( "we run (-2/3).add(2/3) (additive inverse)" ) {
-            testAdditiveInverse1.add(new bffrac::RationalNum(2,3));
+            auto testx0 = bffrac::RationalNum(2,3);
+            testAdditiveInverse1.add(&testx0);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testAdditiveInverse1.positive());
@@ -267,9 +273,9 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         }
 
         auto testAdditiveInverse2 = bffrac::RationalNum(-2,3);
-
         WHEN( "we run (-2/3).add(16/24) (additive inverse)" ) {
-            testAdditiveInverse2.add(new bffrac::RationalNum(16,24));
+            auto testx01 = bffrac::RationalNum(16,24);
+            testAdditiveInverse2.add(&testx01);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testAdditiveInverse2.positive());
@@ -281,7 +287,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac1 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).add(4/5)" ) {
-            testFrac1.add(new bffrac::RationalNum(4,5));
+            auto testx1 = bffrac::RationalNum(4,5);
+            testFrac1.add(&testx1);
 
             THEN("we get 2/15") {
                 REQUIRE(testFrac1.positive());
@@ -293,7 +300,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac2 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).add(-4/5)" ) {
-            testFrac2.add(new bffrac::RationalNum(-4,5));
+            auto testx2 = bffrac::RationalNum(-4,5);
+            testFrac2.add(&testx2);
 
             THEN("we get -22/15") {
                 REQUIRE(!testFrac2.positive());
@@ -305,8 +313,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
         auto testFrac3 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).add(0/13)" ) {
-            auto testFrac3add = bffrac::RationalNum(0,13);
-            testFrac3.add(&testFrac3add);
+            auto testx3 = bffrac::RationalNum(0,13);
+            testFrac3.add(&testx3);
 
             THEN("we get -2/3") {
                 REQUIRE(!testFrac3.positive());
@@ -319,8 +327,8 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
     GIVEN( "5/10" ) {
         auto testNoSideEffect = bffrac::RationalNum(5, 10);
         WHEN("we run (5/10).add(-2/3)") {
-            auto testNoSideEffect_add = new bffrac::RationalNum(-2,3);
-            testNoSideEffect.add(testNoSideEffect_add);
+            auto testNoSideEffect_add = bffrac::RationalNum(-2,3);
+            testNoSideEffect.add(&testNoSideEffect_add);
 
             THEN("we get -1/6") {
                 REQUIRE(!testNoSideEffect.positive());
@@ -329,9 +337,9 @@ SCENARIO( "Sums of rational numbers", "[rationalnum][add]" ) {
             }
 
             AND_THEN("no side effects on -2/3") {
-                REQUIRE(!testNoSideEffect_add->positive());
-                REQUIRE(testNoSideEffect_add->getNumerator() == 2);
-                REQUIRE(testNoSideEffect_add->getDenominator() == 3);
+                REQUIRE(!testNoSideEffect_add.positive());
+                REQUIRE(testNoSideEffect_add.getNumerator() == 2);
+                REQUIRE(testNoSideEffect_add.getDenominator() == 3);
             }
         }
     }
@@ -342,7 +350,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
     GIVEN( "2/3" ) {
         auto testFrac1 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).multiply(4/5)" ) {
-            testFrac1.multiply(new bffrac::RationalNum(4,5));
+            auto testx = bffrac::RationalNum(4,5);
+            testFrac1.multiply(&testx);
 
             THEN("we get 8/15") {
                 REQUIRE(testFrac1.positive());
@@ -353,7 +362,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
 
         auto testFrac2 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).multiply(-4/5)" ) {
-            testFrac2.multiply(new bffrac::RationalNum(-4,5));
+            auto testx = bffrac::RationalNum(-4,5);
+            testFrac2.multiply(&testx);
 
             THEN("we get -8/15") {
                 REQUIRE(!testFrac2.positive());
@@ -364,7 +374,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
 
         auto testFrac3 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).multiply(3/2) (multiplicative inverse)" ) {
-            testFrac3.multiply(new bffrac::RationalNum(3,2));
+            auto testx = bffrac::RationalNum(3,2);
+            testFrac3.multiply(&testx);
 
             THEN("we get 1/1") {
                 REQUIRE(testFrac3.positive());
@@ -375,7 +386,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
 
         auto testFrac4 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).multiply(0,1)" ) {
-            testFrac4.multiply(new bffrac::RationalNum(0,1));
+            auto testx = bffrac::RationalNum(0,1);
+            testFrac4.multiply(&testx);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testFrac4.positive());
@@ -386,7 +398,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
 
         auto testFrac5 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).multiply(-1)" ) {
-            testFrac5.multiply(new bffrac::RationalNum(-1));
+            auto testx = bffrac::RationalNum(-1);
+            testFrac5.multiply(&testx);
 
             THEN("we get -2/3") {
                 REQUIRE(!testFrac5.positive());
@@ -400,7 +413,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
         auto testFrac1 = bffrac::RationalNum(-2, 3);
 
         WHEN( "we run (2/3).multiply(-1)" ) {
-            testFrac1.multiply(new bffrac::RationalNum(-1));
+            auto testx = bffrac::RationalNum(-1);
+            testFrac1.multiply(&testx);
 
             THEN("we get 2/3") {
                 REQUIRE(testFrac1.positive());
@@ -413,7 +427,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
     GIVEN( "0/1" ) {
         auto testFrac1 = bffrac::RationalNum(0,1);
         WHEN("we run (0/1).multiply(4/5)") {
-            testFrac1.multiply(new bffrac::RationalNum(4, 5));
+            auto testx = bffrac::RationalNum(4, 5);
+            testFrac1.multiply(&testx);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testFrac1.positive());
@@ -424,7 +439,8 @@ SCENARIO( "Products of rational numbers", "[rationalnum][multiply]" ) {
 
         auto testFrac2 = bffrac::RationalNum(0,1);
         WHEN("we run (0/1).multiply(-4/5)") {
-            testFrac1.multiply(new bffrac::RationalNum(-4, 5));
+            auto testx = bffrac::RationalNum(-4, 5);
+            testFrac1.multiply(&testx);
 
             THEN("we get nonpositive 0/1") {
                 REQUIRE(!testFrac2.positive());
@@ -439,7 +455,8 @@ SCENARIO( "Quotients of rational numbers", "[rationalnum][divide]" ) {
     GIVEN( "2/3" ) {
         auto testFrac1 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).divide(4/5)" ) {
-            testFrac1.divide(new bffrac::RationalNum(4,5));
+            auto testx = bffrac::RationalNum(4,5);
+            testFrac1.divide(&testx);
 
             THEN("we get 5/6") {
                 REQUIRE(testFrac1.positive());
@@ -450,7 +467,8 @@ SCENARIO( "Quotients of rational numbers", "[rationalnum][divide]" ) {
 
         auto testFrac2 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).divide(-4/5)" ) {
-            testFrac2.divide(new bffrac::RationalNum(-4,5));
+            auto testx = bffrac::RationalNum(-4,5);
+            testFrac2.divide(&testx);
 
             THEN("we get -5/6") {
                 REQUIRE(!testFrac2.positive());
@@ -461,7 +479,8 @@ SCENARIO( "Quotients of rational numbers", "[rationalnum][divide]" ) {
 
         auto testFrac3 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).divide(2/3) (multiplicative inverse)" ) {
-            testFrac3.divide(new bffrac::RationalNum(2,3));
+            auto testx = bffrac::RationalNum(2,3);
+            testFrac3.divide(&testx);
 
             THEN("we get 1/1") {
                 REQUIRE(testFrac3.positive());
@@ -472,7 +491,8 @@ SCENARIO( "Quotients of rational numbers", "[rationalnum][divide]" ) {
 
         auto testFrac5 = bffrac::RationalNum(2,3);
         WHEN( "we run (2/3).divide(-1)" ) {
-            testFrac5.divide(new bffrac::RationalNum(-1));
+            auto testx = bffrac::RationalNum(-1);
+            testFrac5.divide(&testx);
 
             THEN("we get -2/3") {
                 REQUIRE(!testFrac5.positive());
@@ -485,7 +505,8 @@ SCENARIO( "Quotients of rational numbers", "[rationalnum][divide]" ) {
     GIVEN( "0/1" ) {
         auto testFrac1 = bffrac::RationalNum(0, 1);
         WHEN("we run (0/1).divide(-1)") {
-            testFrac1.divide(new bffrac::RationalNum(-1));
+            auto testx = bffrac::RationalNum(-1);
+            testFrac1.divide(&testx);
 
             THEN("we get nonnegative 0/1") {
                 REQUIRE(!testFrac1.positive());
@@ -501,7 +522,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac0 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).subtract(0/5)" ) {
-            testFrac0.subtract(new bffrac::RationalNum(0,5));
+            auto testx = bffrac::RationalNum(0,5);
+            testFrac0.subtract(&testx);
 
             THEN("we get nonegative 0/1") {
                 REQUIRE(!testFrac0.positive());
@@ -513,7 +535,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac1 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).subtract(22/15)" ) {
-            testFrac1.subtract(new bffrac::RationalNum(22,15));
+            auto testx = bffrac::RationalNum(22,15);
+            testFrac1.subtract(&testx);
 
             THEN("we get -22/15") {
                 REQUIRE(!testFrac1.positive());
@@ -525,7 +548,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac2 = bffrac::RationalNum(0,1);
 
         WHEN( "we run (0/1).subtract(-22/15)" ) {
-            testFrac2.subtract(new bffrac::RationalNum(-22,15));
+            auto testx = bffrac::RationalNum(-22,15);
+            testFrac2.subtract(&testx);
 
             THEN("we get 22/15") {
                 REQUIRE(testFrac2.positive());
@@ -539,7 +563,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testAdditiveInverse1 = bffrac::RationalNum(2,3);
 
         WHEN( "we run (2/3).subtract(2/3)" ) {
-            testAdditiveInverse1.subtract(new bffrac::RationalNum(2,3));
+            auto testx0 = bffrac::RationalNum(2,3);
+            testAdditiveInverse1.subtract(&testx0);
 
             THEN("we get nonnegative 0/1") {
                 REQUIRE(!testAdditiveInverse1.positive());
@@ -551,7 +576,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac1 = bffrac::RationalNum(2,3);
 
         WHEN( "we run (2/3).add(4/5)" ) {
-            testFrac1.subtract(new bffrac::RationalNum(4,5));
+            auto testx1 = bffrac::RationalNum(4,5);
+            testFrac1.subtract(&testx1);
 
             THEN("we get -2/15") {
                 REQUIRE(!testFrac1.positive());
@@ -563,7 +589,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac2 = bffrac::RationalNum(2,3);
 
         WHEN( "we run (2/3).subtract(-4/5)" ) {
-            testFrac2.subtract(new bffrac::RationalNum(-4,5));
+            auto testx2 = bffrac::RationalNum(-4,5);
+            testFrac2.subtract(&testx2);
 
             THEN("we get 22/15") {
                 REQUIRE(testFrac2.positive());
@@ -590,7 +617,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testAdditiveInverse1 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).subtract(-2/3)" ) {
-            testAdditiveInverse1.subtract(new bffrac::RationalNum(-2,3));
+            auto testx0 = bffrac::RationalNum(-2,3);
+            testAdditiveInverse1.subtract(&testx0);
 
             THEN("we get nonnegative 0/1") {
                 REQUIRE(!testAdditiveInverse1.positive());
@@ -602,7 +630,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac1 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).subtract(4/5)" ) {
-            testFrac1.subtract(new bffrac::RationalNum(4,5));
+            auto testx1 = bffrac::RationalNum(4,5);
+            testFrac1.subtract(&testx1);
 
             THEN("we get -22/15") {
                 REQUIRE(!testFrac1.positive());
@@ -614,7 +643,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac2 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).subtract(-4/5)" ) {
-            testFrac2.subtract(new bffrac::RationalNum(-4,5));
+            auto testx2 = bffrac::RationalNum(-4,5);
+            testFrac2.subtract(&testx2);
 
             THEN("we get 2/15") {
                 REQUIRE(testFrac2.positive());
@@ -626,8 +656,8 @@ SCENARIO( "Differences of rational numbers", "[rationalnum][subtract]" ) {
         auto testFrac3 = bffrac::RationalNum(-2,3);
 
         WHEN( "we run (-2/3).subtract(0/1)" ) {
-            auto testFrac3add = bffrac::RationalNum(0,1);
-            testFrac3.subtract(&testFrac3add);
+            auto testx3 = bffrac::RationalNum(0,1);
+            testFrac3.subtract(&testx3);
 
             THEN("we get -2/3") {
                 REQUIRE(!testFrac3.positive());
